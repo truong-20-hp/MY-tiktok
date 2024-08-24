f4742f3fd3b0a52bc6e9ccbd6ab55c9ce38b4435
LapTop_XuanTruong <vuxuantruong080920@gmail.com>
2024-08-24 16:24:40 +0700

    cấu hình prettier

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import './App.css';
 import Buttom from '~/component/Buttom/index';
 
 function App() {
-  return (
-    <div className="App">
-      <Buttom/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello anh em
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Buttom />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    hello anh em
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
